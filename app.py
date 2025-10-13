76687163388ab8694b2cf0e3b502ef81d2b3f700
MARUGO-s <pingus0428@gmail.com>
2025-10-13 12:33:58 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2704,7 +2704,7 @@ def save_edited_ingredients():
             if name_key not in request.form:
                 break
             
-                        name = request.form.get(name_key, '')
+            name = request.form.get(name_key, '')
             quantity = float(request.form.get(f'ingredients[{i}][quantity]', 0))
             unit = request.form.get(f'ingredients[{i}][unit]', '')
             capacity = float(request.form.get(f'ingredients[{i}][capacity]', 1))
